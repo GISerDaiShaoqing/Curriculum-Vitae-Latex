05368e6e9550c5d7ce64cdcc7b5747669048618f
GISerDaiShaoqing <dsq1993qingge@163.com>
2019-04-11 10:42:44 +0800

    Update 20190411

--- cn/convertpdf.R
+++ cn/convertpdf.R
@@ -4,3 +4,6 @@ chrome_print("F:/DataScience/CVdaishaoqing/Curriculum-Vitae-Latex/cn/template-zh
 
 system("G:/Study/wkhtmltox/bin/wkhtmltopdf.exe F:/DataScience/CVdaishaoqing/Curriculum-Vitae-Latex/cn/template-zh.html 
        F:/DataScience/CVdaishaoqing/Curriculum-Vitae-Latex/cn/template-zh.pdf")
+
+system("G:/Study/wkhtmltox/bin/wkhtmltopdf.exe http://gisersqdai.top/mycv/
+       F:/DataScience/CVdaishaoqing/Curriculum-Vitae-Latex/cn/encv.pdf")
